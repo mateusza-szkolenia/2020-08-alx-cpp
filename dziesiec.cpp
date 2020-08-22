83076f8ced574f5a7c031e2cef963fc441fde524
Mateusz Adamowski <m.adamowski@alx.pl>
2020-08-22 15:15:14 +0200

    cwiczenie z liczba 10

---
+++ dziesiec.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    double liczba;
+    liczba = 10;
+    std::cout << "Liczba: " << liczba << std::endl;
+    std::cout << "Liczba podwojona: " << 2 * liczba << std::endl;
+    std::cout << "Liczba do kw: " << liczba * liczba << std::endl;
+}
