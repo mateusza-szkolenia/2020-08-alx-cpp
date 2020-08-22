4e3dd98d88684f0cc3489dcac67f067aefd717b2
Mateusz Adamowski <m.adamowski@alx.pl>
2020-08-22 12:31:34 +0200

    dzielenie zmiennoprzecinkowe

---
+++ liczby2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::cout << 2 + 2 * 2 + 2.0 / 4 / 5 + 3 << std::endl;
+    
+    std::cout << 4432234 / 17 << std::endl;
+    std::cout << 4432234 % 17 << std::endl;
+    std::cout << 260719 * 17 + 11 << std::endl;
+}
