389d4b3ca025744f61be02fe28d41d450d912fe9
Mateusz Adamowski <m.adamowski@alx.pl>
2020-08-22 14:38:35 +0200

    pole kuli

---
+++ kula.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    
+    const double PI = 3.1415926;
+    double r = 1.0;
+
+    double pole = 4 * PI * r * r;
+
+    int czyduze;
+
+    std::cout << "Pole powierzchni sfery wynosi: " << pole << std::endl;
+    std::cout << "Pole powierzchni: " << czyduze << std::endl;
+
+}
