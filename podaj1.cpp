0f95c659317d3d0389ce5875ea579ddd5a4d88d9
Mateusz Adamowski <m.adamowski@alx.pl>
2020-08-22 15:15:34 +0200

    obsluga wejscia

---
+++ podaj1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    int a, b;
+
+    std::cout << "Podaj dwie liczby: " << std::endl;
+
+    std::cin >> a;
+    std::cin >> b;
+
+    std::cout << "Suma liczb wynosi " << a + b << std::endl;
+
+}
