ca13b562eb0d94ae0f3194f4898086109f4e987f
Mateusz Adamowski <m.adamowski@alx.pl>
2020-08-22 11:44:10 +0200

    pierwsze pliki

---
+++ prog1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "Dzien " << "dobry" << std::endl;
+
+    return 1;
+
+}
---
+++ prog2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "Dzien " << "dobry" << std::endl;
+    std::cout << "Dzien " << "dobry" << std::endl;
+
+
+}
