87ce7bb7737dcba1f6d39df0a6e4b767a775efaa
Mateusz Adamowski <m.adamowski@alx.pl>
2020-08-23 15:37:09 +0200

    fizz buzz inaczej

--- rozdzial-04/cwx3.cpp
+++ rozdzial-04/cwx3.cpp
@@ -3,20 +3,35 @@
 int main(){
     int i;
 
+    std::string napis;
+
     for ( i = 1; i <= 100; i++ ){
+        napis = "";
+
+
         if ( i % 3 == 0 ){
-            std::cout << "Fizz";
+            napis = "Fizz";
         }
         
         if ( i % 5 == 0 ) {
-            std::cout << "Buzz";
-        }
-        
-        if ( ( i % 3 ) && ( i % 5 ) ){
-            std::cout << i;
+            napis = "Buzz";
         }
 
-         std::cout << std::endl;
+        if ( i % 3 == 0 && i % 5 == 0 ){
+            napis = "Foobar";
+        }
+
+        if ( napis == "" ){
+            std::cout << i;
+        }
+        else {
+            std::cout << napis;
+        }
+
+//      TO NIE ZADZIAŁA, BO RÓŻNE TYPY DANYCH - int i std::string
+//      std::cout << ( napis == "" ) ? i : napis;
+
+        std::cout << std::endl;
 
     }
 
