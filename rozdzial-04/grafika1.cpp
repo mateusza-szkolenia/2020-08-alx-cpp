d52fadfe8c2f435c7835ef1a10178be315e91dc9
Mateusz Adamowski <m.adamowski@alx.pl>
2020-08-23 16:29:30 +0200

    3 paski

--- rozdzial-04/grafika1.cpp
+++ rozdzial-04/grafika1.cpp
@@ -15,8 +15,11 @@ int main(){
         for ( kol = 0; kol < x; kol++ ){
             int r,g,b;
 
-            if ( wiersz % 2 == 0 ){
+            if ( wiersz % 3 == 0 ){
                 r = 255; g = 255; b = 255;
+            }
+            else if ( wiersz % 3 == 1 ){
+                r = 250; g = 10; b = 10;
             }
             else {
                 r = 0; g = 70; b = 230;
