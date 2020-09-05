711b3a44f8f48351544ab2e43708f51a5f3d8006
Mateusz Adamowski <m.adamowski@alx.pl>
2020-09-05 11:34:39 +0200

    funkcje

---
+++ rozdzial-05/funk.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+double f( double x ){
+    double a = 4.0;
+    if ( x > 0 ){
+        return a * x * x;
+    }
+    else {
+        return -x;
+    }
+}
+
+void powitanie(){
+    std::cout << "Witaj w programie prezentujacym funkcje w C++" << std::endl;
+}
+
+
+void dwukrotnosc( int a ){
+    std::cout << "Liczba wynosi: " << a << std::endl;
+    std::cout << "Dwukrotnosc liczby wynosi: " << 2 * a << std::endl;
+}
+
+
+
+int main(){
+    dwukrotnosc( 10 );
+
+/*
+    powitanie();
+    double xx;
+    for ( xx = -3.0; xx < 3.0; xx+=0.25){
+        std::cout << xx << " --> " << f( xx ) << std::endl;
+    }
+*/
+
+}
