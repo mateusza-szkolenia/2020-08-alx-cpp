07c8625a7328dad6bc9dcb64579ab2ac4e022d5c
Mateusz Adamowski <m.adamowski@alx.pl>
2020-09-06 14:36:22 +0200

    max()

--- wskazniki/zadanie2.cpp
+++ wskazniki/zadanie2.cpp
@@ -8,10 +8,12 @@ int suma( int *tab, int n ){
 #include <iostream>
 
 int suma( int *, int );
+int max( int *, int );
 
 int main(){
     int liczby[] = { 1, 7, 90, 3, 45 };
     std::cout << "suma wynosi: " << suma( liczby, 5 ) << std::endl;
+    std::cout << "max wynosi: " << max( liczby, 5 ) << std::endl;
 }
 
 int suma( int *tab, int n ){
@@ -20,4 +22,14 @@ int suma( int *tab, int n ){
         s += tab[i];
     }
     return s;
+}
+
+int max( int *tab, int n ){
+    int m = tab[0];
+    for ( int i = 0; i < n ; i++ ){
+        if ( tab[i] > m ){
+            m = tab[i];
+        }
+    }
+    return m;
 }
