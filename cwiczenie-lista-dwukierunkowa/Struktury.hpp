f72963a9244bb422ae8800a61000f7a0ee0f130d
Mateusz Adamowski <m.adamowski@alx.pl>
2020-09-27 11:04:21 +0200

    rozbudowa typu Listy, dodanie typu ElementListy

--- cwiczenie-lista-dwukierunkowa/Struktury.hpp
+++ cwiczenie-lista-dwukierunkowa/Struktury.hpp
@@ -1,12 +1,22 @@
 #ifndef STRUKTURY_HPP
 #define STRUKTURY_HPP
 
+class ElementListy;
+
 class Lista {
+    int licznik;
+    ElementListy *first;
+    ElementListy *last;
 
     public:
         void pokaz() const;
         bool is_empty() const;
 
 };
+
+class ElementListy {
+    Lista *lista;
+};
+
 
 #endif
